e4a4759ce64e1a7752f319bcbe7c97d46e188616
Fera 13 <fera6055@gmail.com>
2023-03-22 05:16:27 +0100

    Fixed bugs/book items display and added sorters

--- js/booksFunctions.js
+++ js/booksFunctions.js
@@ -1,65 +1,83 @@
-import { fillCatagoryFilters, fillAuthorFilters, fillPriceFilters } from './filters.js';
-
+//import { fillCatagoryFilters, fillAuthorFilters, fillPriceFilters } from './filters.js';
+//import { fillPriceSort, fillAuthorSort, fillTitleSort } from './sorters.js';
 let books;
 
 export async function start() {
-  if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
-  } else {
-    books = await (await fetch('/json/books.json')).json();
-    filterBooks()
-    displayBooks(books);
-    fillCatagoryFilters(books)
-    fillAuthorFilters(books)
-    fillPriceFilters()
+  books = await (await fetch("/json/books.json")).json();
 
-    const categoryFilter = document.querySelector('#category-filter');
-    const priceFilter = document.querySelector('#price-filter');
-    const authorFilter = document.querySelector('#author-filter');
-    
-    categoryFilter.addEventListener('change', filterBooks);
-    priceFilter.addEventListener('change', filterBooks);
-    authorFilter.addEventListener('change', filterBooks);
-  }
+  displayBooks(books);
+
+  fillCatagoryFilters(books);
+  fillAuthorFilters(books);
+  fillPriceFilters();
+
+  fillSorts();
+
+  filterAndSortBooks();
+
+  /*document.addEventListener('DOMContainerLoaded', filterAndSortBooks);
+  document.addEventListener('DOMContainerLoaded', fillCatagoryFilters);
+  document.addEventListener('DOMContainerLoaded', fillAuthorFilters);
+  document.addEventListener('DOMContainerLoaded', fillPriceFilters);
+  
+  document.addEventListener('DOMContainerLoaded', fillSorts);*/
 }
 
-function filterBooks() {
-  const categoryFilter = document.querySelector('#category-filter').value;
-  const priceFilter = document.querySelector('#price-filter').value;
-  const authorFilter = document.querySelector('#author-filter').value;
+function filterAndSortBooks() {
+  const categoryFilter = document.querySelector("#category-filter").value;
+  const priceFilter = document.querySelector("#price-filter").value;
+  const authorFilter = document.querySelector("#author-filter").value;
+  const allSort = document.querySelector("#all-sorts").value;
 
-  let filteredCataBooks = books.filter(book => {
-    if (categoryFilter !== 'all' && book.catagory !== categoryFilter) {
+  let filteredCataBooks = books.filter((book) => {
+    if (categoryFilter !== "all" && book.catagory !== categoryFilter) {
       return false;
     }
 
-    if (priceFilter !== 'all') {
-      const priceRange = priceFilter.split('-');
+    if (priceFilter !== "all") {
+      const priceRange = priceFilter.split("-");
       if (book.price < priceRange[0] || book.price > priceRange[1]) {
         return false;
       }
     }
 
-    if (authorFilter !== 'all' && book.author !== authorFilter) {
+    if (authorFilter !== "all" && book.author !== authorFilter) {
       return false;
     }
     // book passes all filters
     return true;
   });
-  displayBooks(filteredCataBooks);
+
+  let sortedBooks = filteredCataBooks.slice();
+  if (allSort === "titleAscending") {
+    sortedBooks.sort((a, b) => (a.title > b.title ? 1 : -1));
+  } else if (allSort === "titleDescending") {
+    sortedBooks.sort((a, b) => (a.title < b.title ? 1 : -1));
+  } else if (allSort === "priceAscending") {
+    console.log(sortedBooks);
+    sortedBooks.sort((a, b) => a.price - b.price);
+  } else if (allSort === "priceDescending") {
+    sortedBooks.sort((a, b) => b.price - a.price);
+  } else if (allSort === "authorAscending") {
+    sortedBooks.sort((a, b) => (a.author > b.author ? 1 : -1));
+  } else if (allSort === "authorDescending") {
+    sortedBooks.sort((a, b) => (a.author < b.author ? 1 : -1));
+  }
+
+  displayBooks(sortedBooks);
 }
 
 function displayBooks(booksToShow) {
-  console.log(booksToShow)
+  const shopRow = document.querySelector("#shop-row");
   let shopRowContents = ``;
   for (let i = 0; i < booksToShow.length; i++) {
     shopRowContents += `
-      <div class="book-col col-5 col-sm-4 col-lg-3 col-xxl-2">
+      <div class="book-col col-5 col-sm-4 col-lg-3 col-xxl-3">
         <div class="book align-self-stretch border border-primary p-2 mb-2 rounded-2 border-opacity-15">    
           <img class="object-fit-cover photo mb-4 border rounded-4 border-dark-subtle"
             src="https://source.unsplash.com/random/?books">
           <section class="title text-wrap text-break">
-            <a href="/description">${booksToShow[i].title}<a>
+            <a class="overflow-hide fw-semibold" href="/description">${booksToShow[i].title}</a>
           </section>
           <section class="author">
             <span> Author: ${booksToShow[i].author}</span>
@@ -72,7 +90,63 @@ function displayBooks(booksToShow) {
             <button class="buybtn rounded float-end border-primary border-opacity-15">Buy</button>
           </section>
         </div>
-      </div>`
+      </div>`;
   }
-  document.querySelector('.shop-row').innerHTML = shopRowContents
+  shopRow.innerHTML = shopRowContents;
+}
+
+export function fillCatagoryFilters(books) {
+  const catagorySet = new Set();
+
+  for (let i = 0; i < books.length; i++) {
+    catagorySet.add(books[i].catagory);
+  }
+  let bookCatagories = `<option value="all">All</option>`;
+  catagorySet.forEach(function (value) {
+    bookCatagories += `
+        <option value="${value}">${value}</option>`;
+  });
+  const categoryFilter = document.querySelector("#category-filter");
+  categoryFilter.innerHTML = bookCatagories;
+  categoryFilter.addEventListener("change", filterAndSortBooks);
+}
+
+export function fillAuthorFilters(books) {
+  const authorSet = new Set();
+  for (let i = 0; i < books.length; i++) {
+    authorSet.add(books[i].author);
+  }
+  let bookAuthors = `<option value="all">All</option>`;
+  authorSet.forEach(function (value) {
+    bookAuthors += `
+        <option value="${value}">${value}</option>`;
+  });
+  const authorFilter = document.querySelector("#author-filter");
+  authorFilter.innerHTML = bookAuthors;
+  authorFilter.addEventListener("change", filterAndSortBooks);
+}
+
+export function fillPriceFilters() {
+  let bookPrice = `<option value="all">All</option>`;
+  bookPrice += `
+      <option value="$0-150">$0 - $150</option>
+      <option value="150-300">$150 - $300</option>
+      <option value="300-500">$300 - $500</option>
+      <option value="500-2000">$500 - $2000</option>`;
+  const priceFilter = document.querySelector("#price-filter");
+  priceFilter.innerHTML = bookPrice;
+  priceFilter.addEventListener("change", filterAndSortBooks);
+}
+
+export function fillSorts() {
+  let insideSort = `
+        <option value="priceAscending">Price ascending</option>
+        <option value="priceDescending">Price descending</option>
+        <option value="authorAscending">Author ascending</option>
+        <option value="authorDescending">Author descending</option>
+        <option value="titleAscending">Title ascending</option>
+        <option value="titleDescending">Title descending</option>`;
+  const allSort = document.querySelector("#all-sorts");
+  allSort.innerHTML = insideSort;
+  allSort.addEventListener("change", filterAndSortBooks);
 }
--- js/shoppingFunctions.js
+++ js/shoppingFunctions.js
@@ -1,34 +1,30 @@
-
 export function addToCart(event) {
-    if (document.readyState == 'loading') {
-        document.addEventListener('DOMContentLoaded', ready)
-    } else {
-        let btn = event.target.closest('.buybtn');
-        let parent = btn.parentElement.parentElement.parentElement
-        let bookName = parent.querySelector('.title').innerText
-        console.log(bookName)
-        let price = parent.querySelector('.price').innerText
-        price = price.replace('$', '')
-        price = price.replace('Price: ', '')
-        console.log(price)
-        let numPrice = parseFloat(price)
-        console.log(numPrice)
-        addItemToCart(bookName, numPrice)
-        updateSubtotalTotal()
-    }
+  if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready);
+  } else {
+    let btn = event.target.closest(".buybtn");
+    let parent = btn.parentElement.parentElement.parentElement;
+    let bookName = parent.querySelector(".title").innerText;
+    let price = parent.querySelector(".price").innerText;
+    price = price.replace("$", "");
+    price = price.replace("Price: ", "");
+    let numPrice = parseFloat(price);
+    addItemToCart(bookName, numPrice);
+    updateSubtotalTotal();
+  }
 }
 
 function addItemToCart(title, price) {
-    let cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    let cartItems = document.querySelector('.cart-items')
-    let cartItemNames = cartItems.getElementsByClassName('book-name')
-    for (let i = 0; i < cartItemNames.length; i++) {
-        if (cartItemNames[i].innerText == title) {
-            return
-        }
+  let cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row");
+  let cartItems = document.querySelector(".cart-items");
+  let cartItemNames = cartItems.getElementsByClassName("book-name");
+  for (let i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      return;
     }
-    let cartRowContents = `
+  }
+  let cartRowContents = `
         <div class="cart-item cart-column text-wrap text-break" style="width: 140px">
             <span class="book-name">${title}</span>
         </div>
@@ -36,38 +32,39 @@ function addItemToCart(title, price) {
         <div class="book-amount cart-column">
             <input class="book-amount-input" type="number" value="1">
         </div>
-        <span class="book-subTotal cart-column">${price}$</span>`
-    cartRow.innerHTML = cartRowContents
-    cartItems.append(cartRow)
-    updateSubtotalTotal()
-    cartRow.querySelector('.book-amount-input').addEventListener('change', changeAmount)
-    
+        <span class="book-subTotal cart-column">${price}$</span>`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  updateSubtotalTotal();
+  cartRow
+    .querySelector(".book-amount-input")
+    .addEventListener("change", changeAmount);
 }
 
 function changeAmount(event) {
-    let input = event.target
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1
-    }
-    updateSubtotalTotal()
+  let input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateSubtotalTotal();
 }
 
 function updateSubtotalTotal() {
-    let cartItemContainer = document.querySelector('.cart-items')
-    let cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    let total = 0
-    let subTotal = 0
-    for (let i = 0; i < cartRows.length; i++) {
-        let cartRow = cartRows[i]
-        let priceElement = cartRow.querySelector('.book-price')
-        let amountElement = cartRow.querySelector('.book-amount-input')
-        let subTotalElement = cartRow.querySelector('.book-subTotal')
-        let price = parseFloat(priceElement.innerText.replace('$', ''))
-        let amount = amountElement.value
-        let itemSubTotal = price * amount
-        subTotal = subTotal + itemSubTotal
-        subTotalElement.innerText = '$' + itemSubTotal.toFixed(2)
-    }
-    total = Math.round(subTotal * 100) / 100
-    document.querySelector('.total').innerText = '$' + total.toFixed(2)
+  let cartItemContainer = document.querySelector(".cart-items");
+  let cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  let total = 0;
+  let subTotal = 0;
+  for (let i = 0; i < cartRows.length; i++) {
+    let cartRow = cartRows[i];
+    let priceElement = cartRow.querySelector(".book-price");
+    let amountElement = cartRow.querySelector(".book-amount-input");
+    let subTotalElement = cartRow.querySelector(".book-subTotal");
+    let price = parseFloat(priceElement.innerText.replace("$", ""));
+    let amount = amountElement.value;
+    let itemSubTotal = price * amount;
+    subTotal = subTotal + itemSubTotal;
+    subTotalElement.innerText = "$" + itemSubTotal.toFixed(2);
+  }
+  total = Math.round(subTotal * 100) / 100;
+  document.querySelector(".total").innerText = "$" + total.toFixed(2);
 }
